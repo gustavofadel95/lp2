bccd448ea34f6d7298fd0c669302807088d634c4
gustavofadel95 <gustavo.fadel95@gmail.com>
2017-08-31 21:08:03 -0300

    teste

---
+++ Exercicio 1/Exercicio 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int soma= 0;
+
+            for (int i = 0; i <= 1000; i++)
+            {
+              soma= soma + x;
+              x++;
+            }
+            Console.WriteLine(soma);
+            Console.ReadKey();
+        }
+        
+    }
+}
